020f2dbb774e8cfc893b8cb1a86aa07af050d6a7
jerome <jbeureux@cbhbank.com>
2020-05-08 08:46:26 +0200

    imporve connection parameters with multiple configuration

--- src/redis/ConParam.ts
+++ src/redis/ConParam.ts
@@ -1,10 +1,43 @@
-import { ConParamInterface } from "./ConParamInterface";
+import { ConParamInterface, ConfigTypes } from "./ConParamInterface";
 import { IpAddr } from "@app/common/valueobjects/IpAddr"
 import { Port } from "@app/common/valueobjects/Port"
 
 export class ConParam implements ConParamInterface {
 
-  constructor(readonly host: IpAddr, readonly port: Port) {
+  private static NullParam = class extends ConParam {
+      constructor() {
+          super();
+      }
+  }
+
+  static readonly Null: ConParamInterface = new ConParam.NullParam();
+
+  readonly host: IpAddr;
+  readonly port: Port;
+  readonly url: string;
+
+  readonly configType: ConfigTypes;
+
+  constructor(host: IpAddr, port: Port);
+  constructor(url: string);
+  constructor();
+  constructor(...args: any) {
+    if (typeof args[0] === IpAddr && typeof args[1] === Port) {
+      this.host = args[0];
+      this.port = args[1];
+      this.url = "";
+      this.configType = ConfigTypes.ip_and_port;
+    } else if (typeof args[0] === string) {
+      this.host = IpAddr.Null;
+      this.port = Port.Null;
+      this.url = args[0];
+      this.configType = ConfigTypes.url;
+    } else {
+      this.host = IpAddr.Null;
+      this.port = Port.Null;
+      this.url = "";
+      this.configType = ConfigTypes.empty;
+    }
   }
 
 }
--- src/redis/ConParamInterface.ts
+++ src/redis/ConParamInterface.ts
@@ -4,4 +4,12 @@ import { Port } from "@app/common/valueobjects/Port"
 export interface ConParamInterface {
   readonly host: IpAddr;
   readonly port: Port;
+  readonly url: string;
+  readonly configType: ConfigTypes;
 }
+
+export enum ConfigTypes {
+  ip_and_port,
+  url,
+  empty,
+} 
