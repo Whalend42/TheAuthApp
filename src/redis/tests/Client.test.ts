e4953e0b59c527f16a078f43671dfa5f14014e83
jerome <jbeureux@cbhbank.com>
2020-05-08 15:26:08 +0200

    improve tests to match with improvments in classes

--- src/redis/tests/Client.test.ts
+++ src/redis/tests/Client.test.ts
@@ -1,7 +1,8 @@
-import { Client } from "./../Client";
-import { ConParam } from "./../ConParam";
-import { IpAddr } from "../../common/valueobjects/IpAddr";
-import { Port } from "../../common/valueobjects/Port";
+import { Client } from "redis/Client";
+import { ConParam } from "redis/ConParam";
+import { ConfigTypes } from "redis/ConParamInterface";
+import { IpAddr } from "common/valueobjects/IpAddr";
+import { Port } from "common/valueobjects/Port";
 
 test("check IpAddr type assertion", () => {
   const ip = new IpAddr("1.1.1.1");
@@ -19,16 +20,40 @@ test("check Port type assertion", () => {
   }).toThrow();
 });
 
-test("ConParam can be instantiated correctly", () => {
+test("ConParam can be instantiated with port and ip", () => {
   const ip = new IpAddr("127.0.0.1");
   const port = new Port(6379);
   const conParam = new ConParam(ip, port);
   expect(conParam.host.value).toBe("127.0.0.1");
   expect(conParam.port.value).toBe(6379);
+  expect(conParam.configType).toBe(ConfigTypes.ip_and_port);
 });
 
-test("Connect client", () => {
-  const conParam = new ConParam(new IpAddr("127.0.0.1"), new Port(6379));
+test("ConParam can be instantiated url", () => {
+  const url = 'some://url:port';
+  const conParam = new ConParam(url);
+  expect(conParam.host).toBe(IpAddr.Null);
+  expect(conParam.port).toBe(Port.Null);
+  expect(conParam.url).toBe(url);
+  expect(conParam.configType).toBe(ConfigTypes.url);
+});
+
+test("Valid connection client work", async () => {
+  jest.setTimeout(1000 * 10);
+  const conParam = new ConParam("redis://redis:6379");
   const client = new Client(conParam);
-  client.connect();
+  await client.connect();
+  await client.setStr("foo","bar");
+  const result = await client.getStr("foo");
+  expect(result).toBe("bar");
+  await client.quit();
+});
+
+test("Invalid connection client fail", async () => {
+  jest.setTimeout(1000 * 10);
+  const conParam = new ConParam("redis://bar:2222");
+  const client = new Client(conParam);
+  expect(async () => {
+    await client.connect();
+  }).toThrow();
 });
