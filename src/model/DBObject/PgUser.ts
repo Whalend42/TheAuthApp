d0276ded0d6b8c0f936eb2a77a3e9eb4eb3640ce
jerome <jbeureux@cbhbank.com>
2020-05-19 18:52:46 +0200

    improve error handeling and parametrization of class PgUser

--- src/model/DBObject/PgUser.ts
+++ src/model/DBObject/PgUser.ts
@@ -4,6 +4,7 @@ export class PgUser implements User {
 
     private _db: any;
     private _id: string;
+    private _table = "test_user";
 
     constructor(db, id: string) {
         this._db = db;
@@ -15,85 +16,61 @@ export class PgUser implements User {
     }
 
     async email(): Promise<string> {
-        try {
-            let result = "";
-            const one = await this._db.oneOrNone('SELECT email FROM test_user WHERE id = $1', this._id);
-            if (one !== null) {
-                result = one.email;
-            } else {
-                throw new Error("?");
-            }
-            return result;
-        } catch (error) {
-            throw new Error("?");
+        let result = "";
+        const one = await this._db.oneOrNone(`SELECT email FROM ${this._table} WHERE id = $1`, this._id);
+        if (one !== null) {
+            result = one.email;
+        } else {
+            throw new Error(`For some reason it was not possible to retrieve email from user: ${this._id}`);
         }
+        return result;
     }
 
     async name(): Promise<string> {
-        try {
-            let result = "";
-            const one = await this._db.oneOrNone('SELECT name FROM test_user WHERE id = $1', this._id);
-            if (one !== null) {
-                result = one.name;
-            } else {
-                throw new Error("?");
-            }
-            return result;
-        } catch (error) {
-            throw new Error("?");
+        let result = "";
+        const one = await this._db.oneOrNone(`SELECT name FROM ${this._table} WHERE id = $1`, this._id);
+        if (one !== null) {
+            result = one.name;
+        } else {
+            throw new Error(`For some reason it was not possible to retrieve name from user: ${this._id}`);
         }
+        return result;
     }
 
     async secret(): Promise<string> {
-        try {
-            let result = "";
-            const one = await this._db.oneOrNone('SELECT secret FROM test_user WHERE id = $1', this._id);
-            if (one !== null) {
-                result = one.secret;
-            } else {
-                throw new Error("?");
-            }
-            return result;
-        } catch (error) {
-            throw new Error("?");
+        let result = "";
+        const one = await this._db.oneOrNone(`SELECT secret FROM ${this._table} WHERE id = $1`, this._id);
+        if (one !== null) {
+            result = one.secret;
+        } else {
+            throw new Error(`For some reason it was not possible to retrieve secret from user: ${this._id}`);
         }
+        return result;
     }
 
     async changeName(name: string): Promise<User> {
-        try {
-            let result = "";
-            const r = await this._db.result('UPDATE name SET name = $1 FROM test_user WHERE id = $2', [name, this._id]);
-            if (r.rowCount !== 1) {
-                throw new Error("?");
-            }
-            return this;
-        } catch (error) {
-            throw new Error("?");
+        let result = "";
+        const r = await this._db.result(`UPDATE name SET name = $1 FROM ${this._table} WHERE id = $2`, [name, this._id]);
+        if (r.rowCount !== 1) {
+            throw new Error(`For some reason it was not possible to change name of user: ${this._id}`);
         }
+        return this;
     }
     async changeEmail(email: string): Promise<User> {
-        try {
-            let result = "";
-            const r = await this._db.result('UPDATE email SET email = $1 FROM test_user WHERE id = $2', [email, this._id]);
-            if (r.rowCount !== 1) {
-                throw new Error("?");
-            }
-            return this;
-        } catch (error) {
-            throw new Error("?");
+        let result = "";
+        const r = await this._db.result(`UPDATE email SET email = $1 FROM ${this._table} WHERE id = $2`, [email, this._id]);
+        if (r.rowCount !== 1) {
+            throw new Error(`For some reason it was not possible to change email of user: ${this._id}`);
         }
+        return this;
     }
     async changeSecret(secret: string): Promise<User> {
-        try {
-            let result = "";
-            const r = await this._db.result('UPDATE secret SET secret = $1 FROM test_user WHERE id = $2', [secret, this._id]);
-            if (r.rowCount !== 1) {
-                throw new Error("?");
-            }
-            return this;
-        } catch (error) {
-            throw new Error("?");
+        let result = "";
+        const r = await this._db.result(`UPDATE secret SET secret = $1 FROM ${this._table} WHERE id = $2`, [secret, this._id]);
+        if (r.rowCount !== 1) {
+            throw new Error(`For some reason it was not possible to change secret of user: ${this._id}`);
         }
+        return this;
     }
     isNull(): boolean {
         return false;
